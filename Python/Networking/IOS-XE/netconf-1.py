09edd5cbb0e513237b4fc35118321c8df951cb82
Angel Inglese <43012361+AngelIV23@users.noreply.github.com>
2021-01-06 15:30:39 -0500

    Update for netconf-1.py and others related to Sandbox
    Hi Know ! Hope you are doing well, Would you consider adding a docstring to the files related to sandbox to warn about DEVNET changing constantly the URL and port for Always-On Sandbox Devices?
    In my case, viewing the DEVASC Course at CBT found that the URL and port has changed, for Latest and Recommended version, that would help check the Sandbox, changing the URL to the proper one without doing troubleshooting why is not working properly,
    
    Now it has (but it could change):
    ios-xe-mgmt.cisco.com and port 10000
    sandbox-iosxe-latest-1.cisco.com and port 830
    
    I hope that helps, thanks!!
    PS: Not a native english speaker, if accepted, consider reviewing the writing though.

--- Python/Networking/IOS-XE/netconf-1.py
+++ Python/Networking/IOS-XE/netconf-1.py
@@ -1,7 +1,9 @@
 from ncclient import manager
 
-
-router = {"host": "ios-xe-mgmt-latest.cisco.com", "port": "10000",
+'''Warning: This code was ment for an Always-On device that is constanly changing in the public Devnet Sandbox. 
+Refer to the documentation such as URL/Port/Username/Password in case it changes at: https://devnetsandbox.cisco.com/'''
+          
+router = {"host": "sandbox-iosxe-latest-1.cisco.com", "port": "830",
           "username": "developer", "password": "C1sco12345"}
 
 with manager.connect(host=router["host"], port=router["port"], username=router["username"], password=router["password"], hostkey_verify=False) as m:
